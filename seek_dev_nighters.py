77f921617a03b9ca4a59726513e34f67d21df276
veean <nersesowm@gmail.com>
2017-01-02 03:30:07 +0400

    add skeleton of functionality

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,32 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+import datetime
 
-def get_midnighters():
+API = 'https://devman.org/api/challenges/solution_attempts/'
+
+
+def load_attempts():
+    pages = requests.get(API).json()['number_of_pages']
+    for page in range(pages):
+        parameters = {'pages': page}
+        data_response = requests.get(API, params=parameters).json()
+        for record in data_response:
+            yield record
+
+
+def get_midnighters(devman_adepts):
     pass
 
+
+def get_correct_time(timestamp, timezone):
+    if timestamp is None and timezone is None:
+        return None
+    else:
+        correct_time = datetime.datetime.fromtimestamp(timestamp, pytz.timezone(timezone))
+        return correct_time
+
+
 if __name__ == '__main__':
-  pass
+    parameters = {'pages': 2}
+    req = requests.get(API, params=parameters).json()['records']
+    print(req[1])
