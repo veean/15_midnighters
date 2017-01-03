00a26b9af3da0ba82dbd5c52edc1b748e0db1063
veean <nersesowm@gmail.com>
2017-01-04 01:31:37 +0400

    extend __main__  output, fix yield results

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -6,23 +6,24 @@ API = 'https://devman.org/api/challenges/solution_attempts/'
 
 
 def load_attempts():
-    pages = requests.get(API).json()['number_of_pages']
-    for page in range(pages):
-        parameters_ = {'pages': page}
-        data_response = requests.get(API, params=parameters_).json()
-        for record in data_response:
+    data_response = requests.get(API, params={'page': 1})
+    pages = data_response.json()['number_of_pages']
+    for page in range(2, pages+1):
+        parameters = {'page': page}
+        for record in data_response.json()['records']:
             yield record
+        data_response = requests.get(API, params=parameters)
 
 
 def get_midnighters(devman_adepts):
     for adept in devman_adepts:
         correct_date_time = get_correct_time(adept['timestamp'], adept['timezone'])
-        if correct_date_time.hour in (0, 5):
-            yield adept['username']
+        if correct_date_time and correct_date_time.hour in (1, 5):
+            yield adept['username'], correct_date_time
 
 
 def get_correct_time(timestamp, timezone):
-    if timestamp is None and timezone is None:
+    if timestamp is None or timezone is None:
         return None
     else:
         correct_time = datetime.datetime.fromtimestamp(timestamp, pytz.timezone(timezone))
@@ -30,6 +31,6 @@ def get_correct_time(timestamp, timezone):
 
 
 if __name__ == '__main__':
-    parameters = {'pages': 2}
-    req = requests.get(API, params=parameters).json()['records']
-    print(req[1])
+    print('Midnighters submit list: ')
+    for midnighter, submit_time in get_midnighters(load_attempts()):
+        print('Devman adept {} submits challenge at {}'.format(midnighter, submit_time.strftime('%d-%m-%Y %H:%M:%S')))
